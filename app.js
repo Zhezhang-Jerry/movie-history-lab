db28d88cf5909b7aa0ff6ccc3818a7374fc35560
JerryZhang <zhangzhe941020@yahoo.com>
2021-09-14 17:33:29 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// Add DOM selectors to target input and UL movie list
+var inp = document.querySelector("input");
+var myMovieList = document.querySelector("ul");
+const History = document.getElementById("movieHist");
+const myObject = {};
+
+// Example of a simple function that clears the input after a user types something in
+function clearInput() {
+    inp.value = "";
+}
+
+function clearMovies() {
+    // To delete all children of the <ul></ul> (meaning all <li>'s)..we can wipe out the <ul>'s innerHTML
+    myMovieList.innerHTML = '';
+}
+
+// This function is executed when the user clicks [ADD MOVIE] button.
+function addMovie() {
+    // Step 1: Get value of input
+    var userTypedText = inp.value;
+    if (userTypedText == ""){
+        alert("Please enter the movie name!")
+        return; 
+    }
+    // Step 2: Create an empty <li></li>
+    var li = document.createElement("li"); // <li></li>
+
+    // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
+    var textToInsert = document.createTextNode(userTypedText);
+
+    // Step 4: Insert text into li
+    // <li>Harry Potter </li>
+    li.appendChild(textToInsert);
+
+    // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
+    myMovieList.appendChild(li);
+
+    // Step 6: Call the clearInput function to clear the input field
+    clearInput();
+    movieHistory(userTypedText);
+}
+
+function movieHistory(userTypedText) {
+    if (!(userTypedText in myObject)) {
+        myObject[userTypedText] = 1;
+        const li = document.createElement("li");
+        let textNote = document.createTextNode(`${myObject}`)
+        li.appendChild(textNote)
+        History.appendChild(li);
+    }
+    else {
+        myObject[userTypedText] ++;
+
+    }
+    console.log(myObject)
+}
