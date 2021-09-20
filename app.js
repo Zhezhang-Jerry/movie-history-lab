4ea1618f43cee130a351d53e529d28ad40b03003
JerryZhang <zhangzhe941020@yahoo.com>
2021-09-19 21:12:06 -0700

    create design for movie history table in css, use onload function for display local storage movie

--- app.js
+++ app.js
@@ -2,10 +2,62 @@
 const inp = document.querySelector("input");
 const myMovieList = document.querySelector("ul");
 const History = document.getElementById("movieHist");
-const myArray = [];
-const upperArray = [];
-const myObject = {};
 
+// set the <table> in MovieList section and initiate the frist row of the table which is Title and Wathced
+const table = document.createElement("table");
+History.appendChild(table);
+const firstTr = document.createElement("tr");
+table.appendChild(firstTr);
+rowHtml = `
+<td>Title</td>    
+<td>Watched</td>
+`
+firstTr.insertAdjacentHTML('afterbegin', rowHtml)
+
+
+// get the local storage and show them under Movie History section
+window.onload = function(){
+    myArray = JSON.parse(window.localStorage.getItem("user1"))
+    myObject = JSON.parse(window.localStorage.getItem('user2'));
+    if (myArray != null) {
+        for (let movieName of myArray) {
+            let li = document.createElement("li");
+            li.setAttribute("id", movieName)
+            let textToInsert = document.createTextNode(movieName);
+            li.appendChild(textToInsert);
+            myMovieList.appendChild(li);
+            console.log(myArray)
+        }
+    }else {
+        myArray = [];
+    }
+
+    if (myObject !== null){
+        for (key in myObject){
+            const tr = document.createElement("tr");
+            tr.setAttribute("id", "hist" + key);
+            table.appendChild(tr);
+    
+            rowHtml = `
+            <td>${key}</td>    
+            <td>${myObject[key]}</td>
+            `
+            tr.insertAdjacentHTML('afterbegin', rowHtml);
+            console.log(myObject);
+        }
+    }else{
+        myObject = {};
+    }
+    // if (window.localStorage != null) {
+    //     for (let movieName of storageMoviearray) {
+    //     let li = document.createElement("li");
+    //     li.setAttribute("id", movieName)
+    //     let textToInsert = document.createTextNode(movieName);
+    //     li.appendChild(textToInsert);
+    //     myMovieList.appendChild(li);
+    //     console.log(storageMoviearray, storageMovieobject)
+    // }}
+}
 
 // Example of a simple function that clears the input after a user types something in
 function clearInput() {
@@ -27,7 +79,6 @@ function addMovie() {
     }
     if (!(myArray.includes(userTypedText))) { 
         myArray.push(userTypedText)
-        upperArray.push(userTypedText.toUpperCase())
         // Step 2: Create an empty <li></li>
         const li = document.createElement("li"); // <li></li>
         li.setAttribute("id", userTypedText)
@@ -46,16 +97,15 @@ function addMovie() {
     // Step 6: Call the clearInput function to clear the input field
     clearInput()
     movieHistory(userTypedText);
+
+    window.localStorage.setItem('user1', JSON.stringify(myArray));
+
     
-    window.localStorage.setItem("myMovie", JSON.stringify(myObject))
+    // window.localStorage.setItem("movieArray", JSON.stringify(myArray));
+    // window.localStorage.setItem("movieObject", JSON.stringify(myObject));
 }
 
-if (window.localStorage == null) {
-    JSON.parse(window.localStorage.getItem('myMovie'));
-    console.log(JSON.parse(window.localStorage.getItem('myMovie'))
-    )
-}
-
+// create a filter event listener to filt the input letter
 const filter = document.getElementById("filter")
 filter.addEventListener("keyup", filterNames);
 
@@ -71,17 +121,42 @@ function filterNames() {
     }
 }
 
+// when user input a moive name, update the object accordingly, 
+// show under Movie History section, and save to local storage 
 function movieHistory(userTypedText) {
     if (!(userTypedText in myObject)) {
         myObject[userTypedText] = 1;
-    }
-    else {
+    }else {
         myObject[userTypedText] ++;
-        document.getElementById("hist" + userTypedText).remove()
-    }
-    const li = document.createElement("li");
-    li.setAttribute("id", "hist" + userTypedText);
-    let textNote = document.createTextNode(`${userTypedText}                   ${myObject[userTypedText]}`)
-    li.appendChild(textNote)
-    History.appendChild(li);
+        document.getElementById("hist" + userTypedText).remove();
+    } 
+    const tr = document.createElement("tr");
+    tr.setAttribute("id", "hist" + userTypedText);
+    table.appendChild(tr);
+    
+    rowHtml = `
+    <td>${userTypedText}</td>    
+    <td>${myObject[userTypedText]}</td>
+    `
+    tr.insertAdjacentHTML('afterbegin', rowHtml);
+    console.log(myObject);
+    
+    window.localStorage.setItem('user2', JSON.stringify(myObject));
 }
+
+
+// function movieHistory(userTypedText) {
+//     if (!(userTypedText in myObject)) {
+//         myObject[userTypedText] = 1;
+//     }
+//     else {
+//         myObject[userTypedText] ++;
+//         document.getElementById("hist" + userTypedText).remove()
+//     }
+//     const li = document.createElement("li");
+//     li.setAttribute("id", "hist" + userTypedText);
+//     let textNote = document.createTextNode(`${userTypedText}                   ${myObject[userTypedText]}`)
+//     li.appendChild(textNote)
+//     History.appendChild(li);
+// }
+
